385f1a8a1e932a21954237a14e0af93daef21111
Vitalii Starchenko <vitaliy.starchenko@gmail.com>
2014-07-11 12:07:16 -0400

    Existing files added

---
+++ DissolMeshRlx.C
@@ -0,0 +1,420 @@
+/* 
+ * Surface relaxation for dissolFoam
+ * */
+
+#include "DissolMeshRlx.H"
+#include <algorithm>
+
+DissolMeshRlx::DissolMeshRlx(const fvMesh& mesh)
+:
+  version(0.2),
+  mesh_(mesh)
+{
+  // get ID of each patch we need
+  wallID   = mesh_.boundaryMesh().findPatchID("walls");
+  inletID  = mesh_.boundaryMesh().findPatchID("inlet");
+  outletID = mesh_.boundaryMesh().findPatchID("outlet");
+  cyclicID = mesh_.boundaryMesh().findPatchID("periodicx");
+
+  // map vetex ID: patch to global
+  wallsToAll  = mesh_.boundaryMesh()[wallID].meshPoints();
+  inletToAll  = mesh_.boundaryMesh()[inletID].meshPoints();
+  outletToAll = mesh_.boundaryMesh()[outletID].meshPoints();
+  cyclicToAll = mesh_.boundaryMesh()[cyclicID].meshPoints();
+
+  // map vertex ID: global to patch
+  forAll(wallsToAll,  i){  allToWalls[wallsToAll[i]]   = i;  }
+  forAll(inletToAll,  i){  allToInlet[inletToAll[i]]   = i;  }
+  forAll(outletToAll, i){  allToOutlet[outletToAll[i]] = i;  }
+  forAll(cyclicToAll, i){  allToCyclic[cyclicToAll[i]] = i;  }
+
+  setUpLists();
+  setUpPairsRlx();
+  setUpPairsConc();  
+}
+
+
+vectorField DissolMeshRlx::wallRelaxation(){
+  vectorField boundaryRelax(mesh_.boundaryMesh()[wallID].localPoints().size(), vector::zero);
+
+  for(std::map<int, std::pair<int, int> >::iterator iter  = neighbZrlx.begin();
+						    iter != neighbZrlx.end();
+					          ++iter){
+    point A0 = mesh_.boundaryMesh()[wallID].localPoints()[iter->first];
+    point A1 = mesh_.points()[iter->second.first];
+    point A2 = mesh_.points()[iter->second.second];
+    boundaryRelax[iter->first] = vertexDisplZ(A0, A1, A2);
+  }
+
+  for(std::map<int, std::pair<int, int> >::iterator iter  = neighbXrlx.begin();
+						    iter != neighbXrlx.end();
+					          ++iter){
+    point A0 = mesh_.boundaryMesh()[wallID].localPoints()[iter->first];
+    point A1 = mesh_.points()[iter->second.first];
+    point A2 = mesh_.points()[iter->second.second];
+    boundaryRelax[iter->first] += vertexDisplX(A0, A1, A2);
+  }
+  
+  return boundaryRelax;
+}
+
+/*#######################################################################################
+ *  * A0 - boundary which has to be moved.
+ *  * A1 - the point above A0
+ *  * A2 - the point below A0
+ *  * it returns a vector displacement
+ *#######################################################################################*/
+vector DissolMeshRlx::vertexDisplZ( point A0, point A1, point A2 ){
+  scalar newZ = ( A1.z() + A2.z() ) / 2.0;
+  
+  point refA;
+  if( newZ < A0.z() ){
+    refA = A2;
+  }
+  else{
+    refA = A1;
+  }
+  
+  vector A0I = refA - A0;
+  
+  scalar z0I = newZ - A0.z();
+  scalar z0IdivG = z0I / A0I.z(); 
+  
+  vector r0( A0I.x()*z0IdivG,
+             A0I.y()*z0IdivG,
+                     z0I);
+  return r0;
+}
+
+/*#######################################################################################
+ *  * the relaxation on the edge in order to keep uniform distribution over X direction
+ *  * A0 - central point
+ *  * A1 - point with x > x0
+ *  * A2 - point with x < x0
+ *#######################################################################################*/
+vector DissolMeshRlx::vertexDisplX( point A0, point A1, point A2 ){
+  scalar newX = ( A1.x() + A2.x() ) / 2.0;
+  
+  point refA;
+  if( newX < A0.x() ){
+    refA = A2;
+  }
+  else{
+    refA = A1;
+  }
+  
+  vector A0I = refA - A0;
+  
+  scalar x0I = newX - A0.x();
+  scalar x0IdivG = x0I / A0I.x(); 
+  
+  vector r0(             x0I,
+             A0I.y()*x0IdivG,
+             A0I.z()*x0IdivG);
+  return r0;
+}
+
+
+
+vectorField DissolMeshRlx::calculateInletDisplacement(vectorField& wallDispl){
+
+  scalar maxdZ = 0;
+  for(std::list<int>::iterator iter  = lcl_wall_list_wallsInletEdges.begin();
+                               iter != lcl_wall_list_wallsInletEdges.end();
+                             ++iter ){
+    vector A = wallDispl[*iter];
+    maxdZ = max( maxdZ, A.z() );    
+  }
+
+
+  for(std::list<int>::iterator iter  = lcl_wall_list_wallsInletEdges.begin();
+                               iter != lcl_wall_list_wallsInletEdges.end();
+                             ++iter ){
+    vector A = wallDispl[*iter];
+    vector dz(0.0, 0.0, maxdZ - A.z());
+          
+    wallDispl[*iter] += dz;
+  }
+
+  vectorField pointDispInlet( mesh_.boundaryMesh()[inletID].localPoints().size(), vector::zero );
+      
+  pointDispInlet.replace( vector::Z, maxdZ);
+  pointDispInlet.replace( vector::Y, 0);
+  pointDispInlet.replace( vector::X, 0);
+
+  return pointDispInlet;
+}
+
+void DissolMeshRlx::fixEdgeConcentration( scalarField& conc ){
+  // exponential extrapolation of the concentration on the edge vertexes
+  const pointField& loc_points = mesh_.boundaryMesh()[wallID].localPoints();
+  // edgeConcentrationFixMap
+  for(std::map<int, std::pair<int, int> >::iterator itr  = edgeConcentrationFixMap.begin();
+                                                    itr != edgeConcentrationFixMap.end();
+                                                  ++itr ){
+    label pnt0 = itr->first;
+    label pnt1 = mesh_.boundaryMesh()[wallID].whichPoint( (itr->second).first  );
+    label pnt2 = mesh_.boundaryMesh()[wallID].whichPoint( (itr->second).second );
+    
+    conc[ pnt0 ] = extrapolateConcentration(loc_points, conc, pnt0, pnt1, pnt2);
+  }
+/*
+  for(std::map<int,int>::iterator itr  = wallCyclicEdgeToEdge.begin();
+                                  itr != wallCyclicEdgeToEdge.end();
+                                ++itr ){
+    
+    Info<<" c1["<<itr->first<<"]: "<< conc[itr->first]
+        <<" c2["<<itr->second<<"]: "<< conc[itr->second]
+        <<endl;
+    
+    scalar c_av = (conc[itr->first] + conc[itr->second]) * 0.5;
+    conc[itr->first] = c_av;
+    conc[itr->second] = c_av;
+  }
+*/
+}
+
+scalar DissolMeshRlx::extrapolateConcentration(const pointField& loc_points,
+                                const scalarField& point_conc,
+                                label pnt0, label pnt1, label pnt2){
+  scalar r02 = mag(  loc_points[pnt0] - loc_points[pnt2]  );
+  scalar r12 = mag(  loc_points[pnt1] - loc_points[pnt2]  );
+  scalar c1 = point_conc[pnt1];
+  scalar c2 = point_conc[pnt2];
+
+  return c2 * std::pow( c1/c2, r02/r12 );
+}
+
+
+
+void DissolMeshRlx::setUpPairsConc(){
+
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // this is the list for extrapolation of the C value to the vertices on the edge 
+  // between side wall and inlet. Pair first is the next vertex in z direction, pair second
+  // is the next to the next neighbor in z direction
+  // @TODO move to function +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  forAll (pe, indp){
+    // list of edges  
+    labelList elist = pe[indp];
+    // check if edge belong to the surface
+    label neighb     = -1; // near neighbor in z
+    label nextneighb = -1; // next near neighbor in z
+    vector posP = mesh_.boundaryMesh()[wallID].localPoints()[indp];// position of the vertex we check
+
+    // this all just for first layer of vertices in z direction
+    //if( posP.z()<0.005 ){
+    //if( lcl_wall_list_wallsInletEdges.find(indp) != lcl_wall_list_wallsInletEdges.end() ){
+    if( std::find(lcl_wall_list_wallsInletEdges.begin(), lcl_wall_list_wallsInletEdges.end(), indp)
+        !=
+        lcl_wall_list_wallsInletEdges.end()
+    ){
+      forAll (elist, ind){
+        label segside = mesh_.edges()[elist[ind]].start();
+        if(segside==wallsToAll[indp]) segside = mesh_.edges()[elist[ind]].end();
+        
+        label neibIDonBound = mesh_.boundaryMesh()[wallID].whichPoint(segside);
+        if ( neibIDonBound!=-1 ){
+          // means segside is next neighbor of indp on the boundary    
+          vector posN = mesh_.points()[segside];// position of a neighbor
+          
+          // @TODO optimize, it may not work in general case
+          if( posN.z()-posP.z()>0.01 ){
+            neighb = segside;
+            
+            labelList nelist = pe[neibIDonBound];
+            // @TODO the same for neighb, DO it recursive lazy boy!!
+            forAll (nelist, ind11){
+              label segside11 = mesh_.edges()[nelist[ind11]].start();
+              if(segside11==wallsToAll[neibIDonBound]) segside11 = mesh_.edges()[nelist[ind11]].end();
+
+              label neibIDonBound11 = mesh_.boundaryMesh()[wallID].whichPoint(segside11);
+              if ( neibIDonBound11!=-1 ){
+                // means segside is next neighbor of neighb on the boundary
+                vector posN11 = mesh_.points()[segside11];// position of a neighbor
+
+                // @TODO optimize, it may not work in general case
+                if( posN11.z()-posN.z()>0.01 ){
+                  nextneighb = segside11;
+                  break;
+                }
+              }
+            }
+            
+            break;
+          }
+        }
+      }
+      
+      edgeConcentrationFixMap[indp] = std::make_pair(neighb, nextneighb);
+    }
+  }
+  
+  // to fix cyclic boundary
+  /*
+  for(std::list<int>::iterator iter  = lcl_wall_list_wallsCyclicEdges.begin();
+                               iter != lcl_wall_list_wallsCyclicEdges.end();
+                             ++iter ){
+  
+    for(std::list<int>::iterator iter1  = lcl_wall_list_wallsCyclicEdges.begin();
+                                 iter1 != lcl_wall_list_wallsCyclicEdges.end();
+                               ++iter1 ){
+
+      if( iter != iter1 
+          && search2IntMapByValue(wallCyclicEdgeToEdge, *iter) == wallCyclicEdgeToEdge.end()
+          && search2IntMapByValue(wallCyclicEdgeToEdge, *iter1) == wallCyclicEdgeToEdge.end()
+          && search2IntMapByKey(wallCyclicEdgeToEdge, *iter) == wallCyclicEdgeToEdge.end()
+          && search2IntMapByKey(wallCyclicEdgeToEdge, *iter1) == wallCyclicEdgeToEdge.end()
+          && ( abs(mesh_.boundaryMesh()[wallID].localPoints()[*iter].y() -
+                  mesh_.boundaryMesh()[wallID].localPoints()[*iter1].y()) < 0.0001)
+          && ( abs(mesh_.boundaryMesh()[wallID].localPoints()[*iter].z() -
+                  mesh_.boundaryMesh()[wallID].localPoints()[*iter1].z()) < 0.0001)
+        ){
+        wallCyclicEdgeToEdge[*iter] = *iter1;
+        break;
+      }
+    }
+  }
+  */
+
+}
+
+std::map<int,int>::iterator DissolMeshRlx::search2IntMapByValue(std::map<int,int>& ii_map, int value){
+  std::map<int,int>::iterator m_iter = ii_map.end();
+
+  for (std::map<int,int>::iterator it = ii_map.begin(); it != ii_map.end(); ++it ){
+    if (it->second == value){
+       m_iter = it;
+    }
+  }
+  
+  return m_iter;
+}
+
+std::map<int,int>::iterator DissolMeshRlx::search2IntMapByKey(std::map<int,int>& ii_map, int key){
+  return ii_map.find(key);
+}
+
+
+void DissolMeshRlx::setUpPairsRlx(){
+  // global edge list
+  const edgeList& e = mesh_.edges();
+  const polyBoundaryMesh& boundary_mesh_list = mesh_.boundaryMesh();
+
+  // set up storage for pointEdges
+  List<SLList<label> > pointEdges(boundary_mesh_list[wallID].meshPoints().size());
+
+  forAll (e, edgeI){
+    label localID1 = boundary_mesh_list[wallID].whichPoint( e[edgeI].start() );
+    label localID2 = boundary_mesh_list[wallID].whichPoint( e[edgeI].end() );
+    // shift patch points
+    if( localID1 >= 0 ) pointEdges[localID1].append(edgeI);
+    if( localID2 >= 0 ) pointEdges[localID2].append(edgeI);
+  }
+ 
+  // sort out the list
+  pe.setSize( pointEdges.size() );
+  forAll (pointEdges, pointI){
+    pe[pointI].setSize(pointEdges[pointI].size());
+
+    label i = 0;
+    for( SLList<label>::iterator curEdgesIter = pointEdges[pointI].begin();
+         curEdgesIter != pointEdges[pointI].end();
+         ++curEdgesIter, ++i ) {
+      pe[pointI][i] = curEdgesIter();
+    }
+  }
+
+  forAll (pe, indp){
+    // list of edges
+    labelList el = pe[indp];
+    // check if edge belong to the surface
+    label intNeighb = -1; // internal neighbor
+    label bouNeighb = -1; // boundary neighbor with z > z[indp]
+    label bouNeighbLow = -1; // boundary neighbor with z < z[indp]
+    
+    // edge relaxation
+    label nBX = -1; //neighbour with x > x0
+    label nLX = -1; //neighbour with x < x0
+
+    vector posP = boundary_mesh_list[wallID].localPoints()[indp];// position of the vertex we check
+
+    label glob_lab = wallsToAll[indp];
+    forAll (el, inde){
+      label segside = mesh_.edges()[el[inde]].start();
+      if(segside==wallsToAll[indp]) segside = mesh_.edges()[el[inde]].end();
+      if ( boundary_mesh_list[wallID].whichPoint(segside)==-1 ){
+        // means segside is next internal neighbor of indp
+        intNeighb = segside;
+      }
+      else{
+        vector posN = mesh_.points()[segside];// position of a neighbor
+        if( posN.z()-posP.z()>0.01 ){
+          bouNeighb = segside;
+          //break;
+        }
+        if( posN.z()-posP.z()<-0.01 ){
+          bouNeighbLow = segside;
+        }
+          
+        // only for edge
+        if(   ( allToWalls.find( glob_lab ) != allToWalls.end() )  
+                &&
+              ( std::abs(posN.z()-posP.z())<0.001 )
+          ){
+          if( posN.x()-posP.x()>0.001 ){
+            nBX = segside;
+          }
+          if( posN.x()-posP.x()<-0.001 ){
+            nLX = segside;
+          }
+        }
+        
+      }
+    }
+    if( allToInlet.find( glob_lab ) == allToInlet.end() && allToOutlet.find( glob_lab ) == allToOutlet.end() ){
+      neighbZrlx[indp] = std::make_pair(bouNeighb, bouNeighbLow);
+    }
+    if( allToCyclic.find( glob_lab ) == allToCyclic.end() ){
+      neighbXrlx[indp] = std::make_pair(nBX, nLX);
+    }
+    nBX = -1; nLX = -1;
+  }
+
+
+}
+
+void DissolMeshRlx::setUpLists(){
+
+  forAll(wallsToAll, ii){
+    label lW = wallsToAll[ii];
+    
+    // create walls-inlet edge list of vertex IDs
+    std::map<int,int>::iterator iI = allToInlet.find( lW );
+    if(iI != allToInlet.end()){
+      glb_list_wallsInletEdges.push_back( iI->second ); //element found;
+      lcl_wall_list_wallsInletEdges.push_back( ii );
+    }    
+    
+    // create walls-outlet edge list of vertex IDs
+    std::map<int,int>::iterator iO = allToOutlet.find( lW );
+    if(iO != allToOutlet.end()){
+      glb_list_wallsOutletEdges.push_back( iO->second ); //element found;
+      lcl_wall_list_wallsOutletEdges.push_back( ii );
+    }
+
+    // create walls-cyclic edge list of vertex IDs
+    std::map<int,int>::iterator iC = allToCyclic.find( lW );
+    if(iC != allToCyclic.end()){
+      glb_list_wallsCyclicEdges.push_back( iC->second ); //element found;
+      lcl_wall_list_wallsCyclicEdges.push_back( ii );
+    }
+
+  }
+
+}
+
+float DissolMeshRlx::get_version(){
+  return version;
+}
+
---
+++ dissolFoam.C
@@ -0,0 +1,341 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    dissolFoam
+
+Description
+    Solves for flow (Stokes) and transport (steady-state) and moves
+    the mesh according to the reactant flux.
+ * 
+ * 
+ *  Modified for OF ext 3.0.
+ *  Surface relaxation added
+ * 
+
+\*---------------------------------------------------------------------------*/
+
+
+
+
+#include "fvCFD.H"
+#include "pointPatchField.H"
+#include "primitivePatchInterpolation.H"
+#include "dynamicFvMesh.H"
+#include "primitivePatch.H"
+
+
+//#include "fvPatchField.H"
+//#include "mixedPointPatchFields.H"
+
+#include "partialSlipModPointPatchFields.H"
+//#include "partialSlipModFvPatchFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#include <list>
+#include <math.h>
+#include <stdlib.h>
+#include <map>
+#include <iostream>
+#include <fstream>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+#include "DissolMeshRlx.H"
+
+
+// #######################################################################################
+// ##     Auxiliary functions
+// #######################################################################################
+
+
+
+int setSubStep(const edgeList& edges, const pointField& points, const scalarField& concField){
+  
+  scalar max_mesh_step = 0;
+  forAll(concField, ii){
+    max_mesh_step = max(max_mesh_step, concField[ii]);
+  }
+      
+  scalar min_edge = edges[0].mag(points);
+  forAll(edges, i){
+    min_edge = min(min_edge, edges[i].mag(points) );
+  }
+  
+  return (max_mesh_step / min_edge + 1);
+}
+
+
+/*
+ #######################################################################################
+ *    Main program body
+ * 
+ * Schematic fracture geometry:
+ * 
+ *           <------ outlet ------>
+ *        ||                         |          |
+ *        ||                         |          |
+ *        ||<----- walls             |          |
+ *        ||                         |          |
+ * z     /  \                   z    |          |
+ * |_ y      <------ inlet -->  |_ x
+ * 
+ * controlDictionary should have subdictionary CONVECTION_DIFFUSION e.g.:
+ * 
+ *      CONVECTION_DIFFUSION{
+ *        convergence                 1e-9;
+ *        maxIter                     2000;
+ *      }
+ *
+ * 
+ * 
+ #######################################################################################
+*/
+int main(int argc, char *argv[])
+{
+  #include "setRootCase.H"
+  #include "createTime.H"
+  #include "createDynamicFvMesh.H"
+  #include "createFields.H"
+  #include "initContinuityErrs.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+  // Get patch ID for boundaries we want to move ("walls" "inlet")
+  label wallID  = mesh.boundaryMesh().findPatchID("walls");
+  label inletID = mesh.boundaryMesh().findPatchID("inlet");
+  
+  // reference to the mesh relaxation object
+  DissolMeshRlx* mesh_rlx = new DissolMeshRlx(mesh);
+
+  /*
+   * run0timestep is used for skipping the Stokes and the convection-diffusion solvers
+   * if timestep is not 0. At the end of each cycle it is set to true.
+   */
+  bool run0timestep = false;
+  if( runTime.value() == 0 ){  run0timestep = true; }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+  // main time loop
+  while (runTime.run())
+  {
+    Info<< "Begin cycle: Time = " << runTime.timeName() << nl << endl;
+    
+    // Skip the Stokes and the convection-diffusion solvers if timestep is not 0
+    // because it was done within the last timestep
+    if( run0timestep )
+    {
+      /*##########################################
+       *   Stokes flow
+       *##########################################*/
+      int counter = 0; // counter for steady state solver iterations
+      while ( true ){
+        counter++;
+
+        #include "readSIMPLEControls.H"
+
+        // initialize values for convergence checks
+        scalar eqnResidual = 1, maxResidual = 0;
+        scalar convergenceCriterion = 0;
+        int maxNumIter = 0;
+        // read convergence criterion from  system/fvSolution section SIMPLE
+        simple.readIfPresent("convergence", convergenceCriterion);
+        simple.readIfPresent("maxIter", maxNumIter);
+
+        // @need_description
+        p.storePrevIter();
+
+        // Pressure-velocity SIMPLE corrector
+        #include "UEqn.H"
+        #include "pEqn.H"
+
+        // check convergence
+        if( maxResidual < convergenceCriterion || counter >= maxNumIter ){
+          Info<< nl <<" Convergence info: maxResidual: "<< maxResidual
+                  <<"  convergence criterion: "<< convergenceCriterion <<nl
+                  << "Stokes flow: ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+                  << " converged in "<< counter<<" steps"
+                  << "  ClockTime = " << runTime.elapsedClockTime() << " s"<< nl << endl;
+          if(counter >= maxNumIter){
+            Info<<" dissolFoam Runtime WARNING:"
+                    " steady state Stokes flow solver did not converge.\n"
+                << "It reached maximum number of iterations"
+                    << "  counter: " << counter
+                    << "  maxNumIter: " << maxNumIter
+                    << nl << endl;
+          }
+          break;
+        }
+        else{
+          // @TODO probably here we should switch relTol to 0
+          Info<< nl << nl <<" Step "<< counter <<"   maxResidual: "<< maxResidual
+                    <<" > "<< convergenceCriterion <<nl<<endl;
+        }
+      }
+
+
+      /*############################################
+      *   Steady-state convection-diffusion solver
+      * ##########################################*/
+      Info << "Steady-state convection-diffusion"<< endl;
+      // define dictionary for convection diffusion solver
+      dictionary conv_diff = mesh.solutionDict().subDict("CONVECTION_DIFFUSION");
+      // initialize values for convergence checks
+      double convCritCD = 0;   // convergence criteria
+      int maxNumIterCD = 0; // maximum number of iterations
+      // read values for convergence checks from the dictionary
+      conv_diff.readIfPresent("convergence", convCritCD);
+      conv_diff.readIfPresent("maxIter", maxNumIterCD);
+      counter = 0; // set iteration counter to 0
+
+      // Steady-state convection-diffusion solver main loop
+      while ( true ){
+        counter++;
+
+        double residual = solve(  fvm::div(phi, C) == fvm::laplacian(D, C)  ).initialResidual();
+        // @TODO make general function for convergence check
+        if( residual < convCritCD ){
+          Info << "Convection-diffusion: ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+               << "  ClockTime = " << runTime.elapsedClockTime() << " s"<< nl 
+               << " Converged in "<< counter<<" steps" << nl << endl;
+          if(counter >= maxNumIterCD){
+            Info<<" dissolFoam Runtime WARNING:"
+                    " Steady-state convection-diffusion solver did not converge.\n"
+                << "It reached maximum number of iterations"
+                    << "  counter: "<< counter
+                    << "  maxNumIterCD: " << maxNumIterCD
+                    << nl << endl;
+          }
+          break;
+        }
+        else{
+          // @TODO probably here we should switch relTol to 0
+          Info<< nl << nl <<" Step "<< counter
+                    <<"  residual: "<< residual
+                    <<" > "<< convCritCD <<nl<<endl;
+        }
+      }
+
+      /*############################################################################
+       *   Write Output data
+       * ###########################################################################*/
+      runTime++;
+      runTime.write();
+      Info<< "Write data, after conv-diff" << nl << endl;
+    }
+    else{
+      Info<< "dissolFoam: Skip the Stokes and the convection-diffusion solvers." << nl << endl;
+    }
+    
+    /*##############################################################################
+    *   Mesh relaxation
+    * ##############################################################################*/
+    
+//  Mesh update 1: 
+    pointVectorField& pointDisplacement = const_cast<pointVectorField&>(
+      mesh.objectRegistry::lookupObject<pointVectorField>( "pointDisplacement" )
+    );
+
+//  Mesh update 2: Calculate new displacements (interpolate C field to points)  
+
+    // interpolate the concentration from cells to wall faces
+    primitivePatchInterpolation patchInterpolator(mesh.boundaryMesh()[wallID]);
+    scalarField pointC = patchInterpolator.faceToPointInterpolate(C.boundaryField()[wallID]);
+
+    // exponential extrapolation of the concentration on the edge vertices
+    scalarField& point_conc = pointC;
+    mesh_rlx->fixEdgeConcentration( point_conc );
+
+//  Mesh update 2.1: Adjust timestep to mesh resolution. UPD: timestep means number of 
+    // time sub steps when we modify boundary using the same concentration field 
+    // in order not to deform mesh too mush.
+    
+    Info<< "Starting calculation of mesh move substeps"<<nl<<endl;
+    
+    int nsubsteps = setSubStep( mesh.edges(), mesh.points(), point_conc );
+    
+    double nsubsteps_inv = 1.0 / double(nsubsteps);
+    
+    scalarField pointCsub = pointC * nsubsteps_inv;
+    
+    Info<< nl << "Number of substeps  "<< nsubsteps << nl << endl;
+    
+    for(int subi = 0; subi < nsubsteps; subi++)
+    {
+      Info<< nl << "Substep nr. "<< (subi+1) << "  of total "<< nsubsteps << endl;
+      
+      vectorField pointDispWall = pointCsub * mesh.boundaryMesh()[wallID].pointNormals(); //*runTime.deltaTValue()
+
+//  Mesh update 2.2: Inlet displacement
+      vectorField& pdw = pointDispWall;
+      vectorField pointDispInlet = mesh_rlx->calculateInletDisplacement(pdw);
+      
+//  Mesh update 4: Update boundary and relax interior mesh
+      Info<< nl << "Update boundary and relax interior mesh" << endl;
+      
+      vectorField &wallDisp = refCast<vectorField>(pointDisplacement.boundaryField()[wallID]);
+      pointDisplacement.boundaryField()[wallID] == wallDisp + pointDispWall;
+
+      vectorField &inletDisp = refCast<vectorField>(pointDisplacement.boundaryField()[inletID]);
+      pointDisplacement.boundaryField()[inletID] == inletDisp + pointDispInlet;
+      
+      mesh.update();
+      partialSlipModPointPatchVectorField &p_dispPatch = 
+                dynamic_cast< partialSlipModPointPatchVectorField& >
+                (pointDisplacement.boundaryField()[inletID]);
+      p_dispPatch.valueFraction() = false;
+      for(int ii=0; ii<5; ii++){  mesh.update();  }
+      p_dispPatch.valueFraction() = true;
+      mesh.update();
+      
+//  Mesh update 5: boundary mesh relaxation
+
+      Info<< nl << "Boundary mesh relaxation" << nl << endl;
+      
+      for(int i=0; i<2; i++){
+        vectorField boundaryRelax = mesh_rlx->wallRelaxation();
+
+        vectorField &wallDispRelx = refCast<vectorField>(pointDisplacement.boundaryField()[wallID]);
+        pointDisplacement.boundaryField()[wallID] == wallDispRelx + boundaryRelax;
+        mesh.update();
+      }
+      
+      Info<< "Mesh update: ExecutionTime = " << runTime.elapsedCpuTime()
+            << " s" << "  ClockTime = " << runTime.elapsedClockTime()
+            << " s" << nl << endl;
+      
+      //runTime++;
+      //runTime.write();
+    }
+  
+    run0timestep = true;
+    Info<< "Process: Time = " << runTime.timeName() << nl << endl;
+  }
+
+  Info << "End" << nl << endl;
+  return 0;
+}
+
+
+// ************************************************************************* //
